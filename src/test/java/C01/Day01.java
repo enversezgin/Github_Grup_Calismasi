c77f7b7b5e71260aa7dadd60fc6990184153e10d
enversezgin <enversezgin81.com>
2023-09-18 11:47:58 +0300

    02

--- src/test/java/C01/Day01.java
+++ src/test/java/C01/Day01.java
@@ -3,6 +3,8 @@ package C01;
 public class Day01 {
     public static void main(String[] args) {
         System.out.println("day01");
+
+        System.out.println("günlük çalışmaya başladık");
     }
 
 }
