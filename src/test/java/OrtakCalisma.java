6582eb155f324a77ed688af059a59029ab914468
MustafaEmin38 <baltatemin@gmail.com>
2023-09-18 12:49:25 +0300

    v2

--- src/test/java/OrtakCalisma.java
+++ src/test/java/OrtakCalisma.java
@@ -1,9 +1,10 @@
 public class OrtakCalisma {
     public static void main(String[] args) {
-        System.out.println("bu bir ortak çalışmadır");
+        System.out.println("Bilginin efendisi olmak için, çalışmanın kölesi olmak şarttır-balzac");
         System.out.println("bu bir ortak çalışmadır1");
         System.out.println("bu bir ortak çalışmadır2");
         System.out.println("bu bir ortak çalışmadır3");
+
 
     }
 }
