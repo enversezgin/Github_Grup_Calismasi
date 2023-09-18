160eef5cbbc5a5dfd8f3b808961de2cd8c70517c
Hatice Türkmen <hatice.turkmen.222@gmail.com>
2023-09-18 12:50:18 +0300

    V6

--- src/test/java/OrtakCalisma.java
+++ src/test/java/OrtakCalisma.java
@@ -1,7 +1,7 @@
 public class OrtakCalisma {
     public static void main(String[] args) {
         System.out.println("bu bir ortak çalışmadır");
-        System.out.println("bu bir ortak çalışmadır1");
+        System.out.println("Hatice");
         System.out.println("bu bir ortak çalışmadır2");
         System.out.println("bu bir ortak çalışmadır3");
 
