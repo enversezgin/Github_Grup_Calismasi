5fc208c5423fd109aee6e151c7a8edb8c3040038
Ensar27 <132489163+Ensar27@users.noreply.github.com>
2023-09-18 12:49:42 +0300

    v00

--- src/test/java/OrtakCalisma.java
+++ src/test/java/OrtakCalisma.java
@@ -1,9 +1,11 @@
 public class OrtakCalisma {
     public static void main(String[] args) {
-        System.out.println("bu bir ortak çalışmadır");
-        System.out.println("bu bir ortak çalışmadır1");
-        System.out.println("bu bir ortak çalışmadır2");
-        System.out.println("bu bir ortak çalışmadır3");
+        System.out.println("bu bir ortak çalışmadır12312312");
+        System.out.println("ensarrrr özerden");
+        System.out.println("arda hocaaaaaam");
+        System.out.println("bu bir ortak çalışmadır123");
+        System.out.println("bu bir ortak çalışmadır223");
+        System.out.println("bu bir ortak çalışmadır31234");
 
     }
 }
