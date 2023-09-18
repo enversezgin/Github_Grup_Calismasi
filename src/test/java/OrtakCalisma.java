07203ec709cadf599100c53b064bab35388270dc
mustafa topal <yahyasami0131@gmail.com>
2023-09-18 12:52:38 +0300

    tpl

--- src/test/java/OrtakCalisma.java
+++ src/test/java/OrtakCalisma.java
@@ -1,8 +1,8 @@
 public class OrtakCalisma {
     public static void main(String[] args) {
         System.out.println("bu bir ortak çalışmadır");
-        System.out.println("bu bir ortak çalışmadır1");
-        System.out.println("bu bir ortak çalışmadır2");
+        System.out.println("scx");
+        System.out.println("bu gr ortak çalışmadır2");
         System.out.println("bu bir ortak çalışmadır3");
 
     }
