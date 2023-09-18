159aaa66dfbff2f3590d10de8b819310c7f89068
Adnan Söylemez <dnnsylmz78@gmail.com>
2023-09-18 11:59:23 +0300

    adnan

--- src/test/java/Adnan.java
+++
@@ -1,8 +0,0 @@
-public class Adnan {
-    public static void main(String[] args) {
-        System.out.println("adnan01");
-        System.out.println("adnan02");
-        System.out.println("adnan03");
-        System.out.println("adnan04");
-    }
-}
